f2bfdaffe9baee2cb6a28d1b82a38ad116f8389f
陳泰宇 <bear0103papa1@gmail.com>
2025-03-27 13:03:35 +0800

    Jack

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Keynotes from './components/Keynotes';
+import Forums from './components/Forums';
+import Speakers from './components/Speakers';
+import Schedule from './components/Schedule';
+import Registration from './components/Registration';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Hero />
+      <Keynotes />
+      <Forums />
+      <Speakers />
+      <Schedule />
+      <Registration />
+      <Footer />
     </div>
   );
 }
